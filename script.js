4347f84c91f5f2f7c767d40f248a040a18cba5a3
Laiba Choudhry <laiba@hotmail.nl>
2023-05-10 17:56:00 +0200

    YESSSSSSSSSS

--- script.js
+++ script.js
@@ -28,8 +28,8 @@
 //   });
 // }
 
-const buttons = document.getElementsByTagName("button");
-const textarea = document.getElementById("my-textarea");
+// const buttons = document.getElementsByTagName("button");
+// const textarea = document.getElementById("my-textarea");
 
 // for (let i = 0; i < buttons.length; i++) {
 //   buttons[i].addEventListener("mouseover", () => {
@@ -49,19 +49,85 @@ const textarea = document.getElementById("my-textarea");
 // });
 
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("mouseover", () => {
-    const text = buttons[i].textContent;
-    navigator.clipboard.writeText(text);
-  });
+// for (let i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("mouseover", () => {
+//     const text = buttons[i].textContent;
+//     navigator.clipboard.writeText(text);
+//   });
   
-  buttons[i].addEventListener("focus", () => {
-    const text = buttons[i].textContent;
-    navigator.clipboard.writeText(text);
-  });
+//   buttons[i].addEventListener("focus", () => {
+//     const text = buttons[i].textContent;
+//     navigator.clipboard.writeText(text);
+//   });
+// }
+
+// textarea.addEventListener("click", async () => {
+//   const text = await navigator.clipboard.readText();
+//   textarea.value = text;
+// });
+
+
+const copyButton = document.getElementById("copy-only");
+const pasteButton = document.getElementById("paste-only");
+const textarea = document.getElementById("my-textarea");
+const message = document.getElementById("message");
+let copiedText = "";
+
+function showMessage(text) {
+  message.innerText = text;
+  setTimeout(function() {
+    message.innerText = "";
+  }, 4000); // hide the message after 4 seconds
 }
 
-textarea.addEventListener("click", async () => {
-  const text = await navigator.clipboard.readText();
-  textarea.value = text;
+function showExplosion() {
+  const explosion = document.createElement("div");
+  explosion.classList.add("explosion");
+  explosion.innerHTML = '<span class="emoji">👍👍👍</span>';
+  document.body.appendChild(explosion);
+  setTimeout(function() {
+    explosion.remove();
+  }, 1000); // remove the explosion after 2 seconds
+}
+
+// Copy using "c" key
+document.addEventListener("keydown", function (event) {
+  if (event.key === "c") {
+    copiedText = document.querySelector("article").textContent;
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(document.querySelector("article"));
+    selection.removeAllRanges();
+    selection.addRange(range);
+    showMessage("De tekst is gekopieerd!");
+  }
+});
+
+// Paste using "v" key
+document.addEventListener("keydown", function (event) {
+  if (event.key === "v") {
+    textarea.value = copiedText;
+    showMessage("De tekst is geplakt!");
+    setTimeout(showExplosion, 2000); // show the explosion after 2 seconds
+  }
+});
+
+copyButton.addEventListener("click", function () {
+  copiedText = document.querySelector("article").textContent;
+  const selection = window.getSelection();
+  const range = document.createRange();
+  range.selectNodeContents(document.querySelector("article"));
+  selection.removeAllRanges();
+  selection.addRange(range);
+  showMessage("De tekst is gekopieerd!");
+});
+
+pasteButton.addEventListener("click", function () {
+  if (copiedText !== "") {
+    textarea.value = copiedText;
+    showMessage("De tekst is geplakt!");
+    setTimeout(showExplosion, 1000); // show the explosion after 2 seconds
+  } else {
+    showMessage("Er is geen tekst gekopieerd om te plakken.");
+  }
 });
